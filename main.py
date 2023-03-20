bec5ce8c714dc29bad6d9ede683e31b82bd3e1a3
Sandy Woods <sandywoods@Sandys-MacBook-Air-2.local>
2023-03-19 21:48:47 -0400

    Data Type

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-name = "James"
-full_name = "J James"
+brand = "Amigoscode"
+age = 2
 pi = 3.14
+numbers = []
+isAdult = True
+
+print(type(brand))
+print(type(age))
+print(type(pi))
+print(type(numbers))
+print(type(isAdult))
