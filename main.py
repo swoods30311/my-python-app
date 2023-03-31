3b6e8bbffe474be2eb4d859a950927da44f660bf
Sandy Woods <sandywoods@Sandys-MacBook-Air-2.local>
2023-03-31 11:29:22 -0400

    multiline And Formatting Strings

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-brand = 'amigoscode'
-print(brand.upper())
-#replace and upper should be consider higher order functions since they're in the print method
-print(brand.replace('o','u').upper())
-print(len(brand))
-print(brand == 'AmiGosCode')
-print(brand != 'AmiGosCode')
-print(brand == 'amigoscode')
-print('mucus' not in brand)
+words = "Sandy " \
+    "Woods"
+print(words)
+
+words1 = """
+Sandy
+Woods
+"""
+print(words1)
+
+email =f"""
+Yellow {words}, 
+how are you?
+It was nice visiting you
+"""
+print(email)
+email =f"""
+Yellow {words}, 
+how are you?
+It was nice visiting you
+age{4*4}, right?
+"""
+print(email)
