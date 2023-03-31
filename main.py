5971b0f22c67897d4bac94055f67214ee5a58d4b
Sandy Woods <sandywoods@Sandys-MacBook-Air-2.local>
2023-03-31 12:09:26 -0400

    Logical Operators

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-print(10 > 5)
-print(10 >= 5)
-print(10 >= 10)
-print(10 > 10)
-print(10 != 10)
+# Java: System.out.println((10 > 5) && (1>3); --> False
+print(10 > 5 and (1 > 3))
+print(10 > 5 and (1 < 3) and 'A'== 'a')
+print(10 > 5 and (1 < 3) or 'A'== 'a')
+print(3 != 3)
