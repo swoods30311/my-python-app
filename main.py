439e649e518696f98b4a14c0fcd7e38994bdd71e
Sandy Woods <sandywoods@Sandys-MacBook-Air-2.local>
2023-04-01 17:56:49 -0400

    List

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-number = 37
-message = f"{number} is even" if number % 2 == 0 else f"{number} is odd"
-print(message)
+print(type([]))
+
+number = 8
+number_second = 2
+number_third = 7
+number_fourth = 4
+
+numbers_and_then_some = [number, number_second, number_third, number_fourth,'c',"Sixteen"]
+print(numbers_and_then_some)
+
+print(numbers_and_then_some[3])
+print(numbers_and_then_some[0])
+print(type(numbers_and_then_some[4]))
+print(type(numbers_and_then_some[5]))
