5a5cd0e550b4f540c9076656eac1960fab2592f2
Sandy Woods <sandywoods@Sandys-MacBook-Air-2.local>
2023-04-01 18:26:48 -0400

    Useful List Methods

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 print(type([]))
 
-number = 8
-number_second = 2
-number_third = 7
-number_fourth = 4
+number = 12
+number_second = 17
+number_third = 46
+number_fourth = 3
 
-numbers_and_then_some = [number, number_second, number_third, number_fourth,'c',"Sixteen"]
-print(numbers_and_then_some)
-
-print(numbers_and_then_some[3])
-print(numbers_and_then_some[0])
-print(type(numbers_and_then_some[4]))
-print(type(numbers_and_then_some[5]))
+numbers = [number, number_second, number_third, number_fourth, 18, 46]
+print(numbers)
+numbers.append(110)
+print(numbers)
+print(len(numbers))
+numbers.sort()
+print(numbers)
+print(110 in numbers)
+print(numbers[0])
+print(type(numbers[4]))
+numbers.clear()
+print(numbers)
