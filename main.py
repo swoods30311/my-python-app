ec68586f2f3db2e5815ac533d766bb9e4b2c9d72
Sandy Woods <sandywoods@Sandys-MacBook-Air-2.local>
2023-04-01 16:55:47 -0400

    If stmts

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-"""brand = "Amigoscode"
-brand = "Nike"
-print(brand)"""
-number = 0
-number += 1 #Same as number = number +1 or number++; in Java
-print(number)
-number += 3
-print(number)
-number **=3 # Guess you said f squares and went str8 to cube 2^3
-print(number)
+number = 100
+if number % 2 == 0:
+    print(f"{number} is even")
+else:
+    print(f"{number} is odd")
