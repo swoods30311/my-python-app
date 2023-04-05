cd5d9ff4863ace38ed7f88debd58aa854753b299
Sandy Woods <sandywoods@Sandys-MacBook-Air-2.local>
2023-04-05 05:05:20 -0400

    Break and Continue

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-firstNumber = 16
-number = 24
-
-while number > firstNumber:
-    print(number)
-    number-= 1
-
-
+a = 1
+while True:
+    print(a)
+    a += 1
+    if a == 5:
+        break
+i = 0
+print() #Give me a lil air- Petey Green
+#Took out the coolest numbers in the bunch :(
+while i < 9:
+    i += 1
+    if i % 3 == 0:
+        continue
+    print(i)
