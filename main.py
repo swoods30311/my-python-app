b23389953997041c38229b45b3a580ed6decbe5a
Sandy Woods <sandywoods@Sandys-MacBook-Air-2.local>
2023-04-05 03:00:00 -0400

    Dictionaries

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
-print(type([]))
-
-number = 12
-number_second = 17
-number_third = 46
-number_fourth = 3
-
-numbers = [number, number_second, number_third, number_fourth, 18, 46]
-print(numbers)
-numbers.append(110)
-print(numbers)
-numbers.remove(number_third)
-print(numbers)
-numbers.pop() #110 was popped. Does this mean Pythons list use stack data structure by default?
-del numbers[0]
-del numbers[1]
-numbers.pop()
-print(numbers)
+#Dictionaries allow storage of K,V pairs
+#Keys (such as name, age and address below MUST BE UNIQUE...SAME AS JAVA)
+person = {
+    "name": "Jamal",
+    "age": 20,
+    "address": "USA"
+}
+print(person["address"])
+print(person.keys())
+print(person.get("age"))
