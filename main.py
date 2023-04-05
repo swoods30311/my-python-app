5634fb972da880e042e6e1ccdf4d85576857f6df
Sandy Woods <sandywoods@Sandys-MacBook-Air-2.local>
2023-04-05 03:23:20 -0400

    For Loop

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-#Dictionaries allow storage of K,V pairs
-#Keys (such as name, age and address below MUST BE UNIQUE...SAME AS JAVA)
-person = {
-    "name": "Jamal",
-    "age": 20,
-    "address": "USA"
-}
-print(person["address"])
-print(person.keys())
-print(person.get("age"))
+names = ["Ahmed","Annah",
+         "James", "Jamila"]
+#come back and debug once you get fire w/ Python
+for name in names:
+    print(name)
