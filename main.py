bbb70f850cb76093ffe2e44b75203ce025893f5c
Sandy Woods <sandywoods@Sandys-MacBook-Air-2.local>
2023-04-05 19:16:12 -0400

    Functions

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-a = 1
-while True:
-    print(a)
-    a += 1
-    if a == 5:
-        break
-i = 0
-print() #Give me a lil air- Petey Green
-#Took out the coolest numbers in the bunch :(
-while i < 9:
-    i += 1
-    if i % 3 == 0:
-        continue
-    print(i)
+name = "Sandy"
+def greeting (name):
+    print(f"Hello {name}")
+
+greeting(name)
